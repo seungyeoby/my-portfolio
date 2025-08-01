0e0074efa1e89a872eab63df3fd76661ac3badf2
tjdux <tjduxxx@gmail.com>
2025-08-01 18:02:39 +0900

    refactor

--- src/Routers/recommendationRouter.ts
+++ src/Routers/recommendationRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import recommendationController from "../controllers/recommendation.controller.js";
+import recommendationController from "../Controllers/recommendation.controller.js";
 
 const router: Router = Router();
 
