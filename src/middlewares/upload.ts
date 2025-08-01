8b10baa3292318a93c4ae506e5fd65781cbd388a
tjdux <tjduxxx@gmail.com>
2025-08-01 19:57:07 +0900

    fix: 타입 추가

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,10 +3,18 @@ import path from "path";
 import { Request } from "express";
 
 const storage = multer.diskStorage({
-  destination: (req: Request, file, cb) => {
+  destination: (
+    req: Request,
+    file: any,
+    cb: (arg0: null, arg1: string) => void
+  ) => {
     cb(null, "public/images");
   },
-  filename: (req: Request, file, cb) => {
+  filename: (
+    req: Request,
+    file: { originalname: string },
+    cb: (arg0: null, arg1: string) => void
+  ) => {
     const ext = path.extname(file.originalname);
     const base = path.basename(file.originalname, ext);
     cb(null, `${base}-${Date.now()}${ext}`);
