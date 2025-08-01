ccb12c98195c377c9c8394113a49b8a7a8280ea0
jihyun1029 <parkji2028@naver.com>
2025-08-01 18:07:49 +0900

    user 수정

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,15 +1,9 @@
 import express from "express";
 import { authenticateToken } from "../middlewares/auth.js";
-import { updateUserValidation, changePasswordValidation, validateNicknameNotExistsForUpdate, validateCurrentPassword } from "../middlewares/validation.js";
+import { changePasswordValidation, validateCurrentPassword } from "../middlewares/validation.js";
 import { UserController } from "../controllers/userController.js";
 
 const router = express.Router();
-
-// 내 정보 조회
-router.get("/profile", authenticateToken, UserController.getProfile);
-
-// 내 정보 수정
-router.put("/profile", authenticateToken, updateUserValidation, validateNicknameNotExistsForUpdate, UserController.updateProfile);
 
 // 비밀번호 변경
 router.put("/password", authenticateToken, changePasswordValidation, validateCurrentPassword, UserController.changePassword);
@@ -17,4 +11,4 @@ router.put("/password", authenticateToken, changePasswordValidation, validateCur
 // 회원 탈퇴
 router.delete("/account", authenticateToken, UserController.deleteAccount);
 
-export default router; 
+export default router;
