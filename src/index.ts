ce2ac2fc032c354645ab4dff99bf01c45d19460d
tjdux <tjduxxx@gmail.com>
2025-08-01 14:08:02 +0900

    fix: error handler 수정

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import recommendationRouter from "./Routers/recommendationRouter.js";
+import errorHandlingMiddleware from "./Middlewares/error-handling.js";
 import cors from "cors";
 
 const app: Express = express();
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/recommendations", recommendationRouter);
+
+app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
