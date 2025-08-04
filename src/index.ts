c6c39a3635c2ace28f69b24d56911c543b02f9ff
seungyeoby <ysy9536@gmail.com>
2025-08-04 23:00:57 +0900

    0804병합

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
 }));
 
